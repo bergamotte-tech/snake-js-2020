d14101c69ddde3ddf169db81528db32781f7943c
JUNUZOVIC SAID p1801045 <said.junuzovic@etu.univ-lyon1.fr>
2020-04-11 15:04:28 +0200

    add config files for levels

--- project/classic-snake/scripts/classes/game/Snake.js
+++ project/classic-snake/scripts/classes/game/Snake.js
@@ -1,17 +1,16 @@
-
 class Snake {
-    constructor(ctx) {
-        this.ctx = ctx;
-        this.x = 0;
-        this.y = 0;
-        this.length = 1;
-        this.color = 'white';
-    };
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.x = 0;
+    this.y = 0;
+    this.length = 1;
+    this.color = "white";
+  }
 
-    draw() {
-        this.ctx.fillStyle = color;
-        this.ctx.fillRect(this.x, this.y, 1, 1);
-    }
+  draw() {
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(this.x, this.y, 1, 1);
+  }
 }
 
 export { Snake as default };
